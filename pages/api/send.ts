8c4f3868478b165356fb82c5ab8d61b93e6ef696
Zeno Rocha <zno.rocha@gmail.com>
2023-06-04 11:03:42 -0700

    feat: Remove extra env vars

--- pages/api/send.ts
+++ pages/api/send.ts
@@ -7,9 +7,10 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   try {
     const data = await resend.sendEmail({
-      from: process.env.EMAIL_FROM || '',
-      to: process.env.EMAIL_TO || '',
-      subject: "hello world",
+      from: 'onboarding@resend.dev',
+      to: 'delivered@resend.dev',
+      subject: "Hello world",
+      html: "<strong>It works!</strong>",
       react: EmailTemplate({ firstName: "John", product: "Resend" }),
     });
 
