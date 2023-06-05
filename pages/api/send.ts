7606d270578eb0e4b6338eeb21a29165922db2eb
Zeno Rocha <zno.rocha@gmail.com>
2023-06-04 18:28:34 -0700

    refactor: Simplify email template

--- pages/api/send.ts
+++ pages/api/send.ts
@@ -1,5 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import EmailTemplate from '../../components/EmailTemplate';
+import { EmailTemplate } from '../../components/EmailTemplate';
 import { Resend } from 'resend';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
@@ -11,13 +11,12 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
       to: 'delivered@resend.dev',
       subject: "Hello world",
       html: "<strong>It works!</strong>",
-      react: EmailTemplate({ firstName: "John", product: "Resend" }),
+      react: EmailTemplate({ firstName: "John" }),
     });
 
     res.status(200).json(data);
   }
   catch (error) {
-    console.error(error);
     res.status(400).json(error);
   }
 }
