e9da387389176b81b290e3931cd39f661c033135
Jayan Ratna <jayanratna@gmail.com>
2023-06-21 16:42:18 +1200

    Update Resend SDK usage

--- pages/api/send.ts
+++ pages/api/send.ts
@@ -6,11 +6,10 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   try {
-    const data = await resend.sendEmail({
-      from: 'onboarding@resend.dev',
-      to: 'delivered@resend.dev',
+    const data = await resend.emails.send({
+      from: 'Acme <onboarding@resend.dev>',
+      to: ['delivered@resend.dev'],
       subject: "Hello world",
-      html: "<strong>It works!</strong>",
       react: EmailTemplate({ firstName: "John" }),
     });
 
