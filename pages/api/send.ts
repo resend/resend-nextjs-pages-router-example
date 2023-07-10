d60ce4bc7023828fa13dda2f0cf9aa5a1deb92bc
Bu Kinoshita <6929565+bukinoshita@users.noreply.github.com>
2023-07-10 11:36:53 -0300

    refactor: Rename file (#4)

--- pages/api/send.ts
+++ pages/api/send.ts
@@ -1,5 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { EmailTemplate } from '../../components/EmailTemplate';
+import { EmailTemplate } from '../../components/email-template';
 import { Resend } from 'resend';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
