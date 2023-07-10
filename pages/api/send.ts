3c519310e8a47c9f09b38407d14848e99efba3fa
Jayan Ratna <jayanratna@gmail.com>
2023-07-11 02:40:12 +1200

    refactor: SDK usage (#3)
    Co-authored-by: Bu Kinoshita <6929565+bukinoshita@users.noreply.github.com>

--- pages/api/send.ts
+++ pages/api/send.ts
@@ -7,11 +7,10 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   try {
     const data = await resend.emails.send({
-      from: 'onboarding@resend.dev',
-      to: 'delivered@resend.dev',
-      subject: "Hello world",
-      html: "<strong>It works!</strong>",
-      react: EmailTemplate({ firstName: "John" }),
+      from: 'Acme <onboarding@resend.dev>',
+      to: ['delivered@resend.dev'],
+      subject: 'Hello world',
+      react: EmailTemplate({ firstName: 'John' }),
     });
 
     res.status(200).json(data);
