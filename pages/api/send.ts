d44a7fbbb6906dc287026d13ee6fb288f8a845e7
Bu Kinoshita <6929565+bukinoshita@users.noreply.github.com>
2023-07-10 11:35:16 -0300

    refactor: Use emails send method (#2)

--- pages/api/send.ts
+++ pages/api/send.ts
@@ -6,7 +6,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   try {
-    const data = await resend.sendEmail({
+    const data = await resend.emails.send({
       from: 'onboarding@resend.dev',
       to: 'delivered@resend.dev',
       subject: "Hello world",
@@ -15,8 +15,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     });
 
     res.status(200).json(data);
-  }
-  catch (error) {
+  } catch (error) {
     res.status(400).json(error);
   }
 }
