9cd7421e228200d9b019d96a115083d307f4a32e
Zeno Rocha <zno.rocha@gmail.com>
2023-11-01 22:30:10 -0700

    chore: Upgrade to Next 14 (#8)

--- pages/api/send.ts
+++ pages/api/send.ts
@@ -6,15 +6,19 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   try {
-    const data = await resend.emails.send({
+    const { data, error } = await resend.emails.send({
       from: 'Acme <onboarding@resend.dev>',
       to: ['delivered@resend.dev'],
       subject: 'Hello world',
       react: EmailTemplate({ firstName: 'John' }),
     });
 
-    res.status(200).json(data);
+    if (error) {
+      res.status(400).json({ error });
+    }
+
+    res.status(200).json({ data });
   } catch (error) {
-    res.status(400).json(error);
+    res.status(400).json({ error });
   }
 }
